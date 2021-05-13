1d299b892b36b0752bdc7a7b22d6580b7015eb02
Davide <d.sadotti@gmail.com>
2021-05-13 17:21:50 +0200

    Esercizio Finito

--- laravel/app/Http/Controllers/MovieController.php
+++ laravel/app/Http/Controllers/MovieController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class MovieController extends Controller
 {
     public function index(){
-        return view('welcome');
+        $movies = Movie::all();
+
+        
+        return view('welcome', [
+            'movies' => $movies
+        ]);
     }
 }
